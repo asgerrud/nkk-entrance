ad47a33cd1c0975f392a0e2214885bfd828cef6d
Asger Rud Hansen <36601572+asgerrud@users.noreply.github.com>
2025-04-23 20:54:55 +0200

    Fix build error

--- app/api/dayticket/route.ts
+++ app/api/dayticket/route.ts
@@ -1,14 +1,14 @@
 export async function GET() {
-  const options = {
-    method: "GET",
-    url: "https://lockoff-api.gnerd.dk/single-dayticket",
-    headers: {
-      accept: "application/json",
-      ext_dayticket_token: process.env.EXT_DAYTICKET_TOKEN,
+  const response = await fetch(
+    "https://lockoff-api.gnerd.dk/single-dayticket",
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        ext_dayticket_token: process.env.EXT_DAYTICKET_TOKEN ?? "",
+      },
     },
-  };
-
-  const response = await fetch(options.url, options);
+  );
 
   const data = await response.json();
 
