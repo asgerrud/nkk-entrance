852605cb2c0eb39334b3f8b489e4f264e752a9ff
Asger Rud Hansen <36601572+asgerrud@users.noreply.github.com>
2025-04-26 16:59:17 +0200

    Check system status before creating checkout session

--- app/ticket/page.tsx
+++ app/ticket/page.tsx
@@ -39,7 +39,7 @@ export default async function TicketPage({
     return (
       <div className="font-mono">
         Failed to create QR code. The entrance system may be down. Please
-        contact <b>{process.env.CONTACT_MAIL}</b> for help.
+        contact <b>{process.env.NEXT_PUBLIC_CONTACT_MAIL}</b> for help.
         <br />
         Please inform us about your ticket id:{" "}
         <b className="text-black font-black">{invoice}</b>
