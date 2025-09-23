f868ce0266a0af0298e57f7eeee1a8e90a2f3e5d
Asger Rud Hansen <36601572+asgerrud@users.noreply.github.com>
2025-09-23 17:09:19 +0200

    Fix opening hours for Friday

--- types/constants.ts
+++ types/constants.ts
@@ -14,8 +14,8 @@ export const GUEST_OPENING_HOURS = [
   { open: "07:00", close: "15:00" }, // Tuesday
   { open: "07:00", close: "15:00" }, // Wednesday
   { open: "07:00", close: "15:00" }, // Thursday
-  { open: "07:00", close: "15:00" }, // Friday
+  { open: "07:00", close: "21:00" }, // Friday
   { open: "09:00", close: "21:00" }, // Saturday
 ];
 
-export const TICKET_STORAGE_KEY = 'nkk_ticket';
+export const TICKET_STORAGE_KEY = "nkk_ticket";
