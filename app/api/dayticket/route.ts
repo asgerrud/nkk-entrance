7011c89d5e45ea33b9a2d7ff0f62c6b59d6962ff
Asger Rud Hansen <asgerrud@gmail.com>
2025-12-27 17:50:36 +0100

    Move QR code generation logic to Next.js

--- app/api/dayticket/route.ts
+++ app/api/dayticket/route.ts
@@ -1,16 +1,44 @@
-export async function GET() {
-  const response = await fetch(
-    "https://lockoff-api.gnerd.dk/single-dayticket",
-    {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        ext_dayticket_token: process.env.EXT_DAYTICKET_TOKEN ?? "",
-      },
-    },
+import crypto from "crypto";
+import { DAYTICKET_CONFIG } from "@/types/constants";
+import base45 from "base45";
+import dayjs from "dayjs";
+import utc from 'dayjs/plugin/utc'
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc)
+dayjs.extend(timezone);
+
+export function generateDayTicket() {
+  const { SECRET, NONCE_SIZE, DIGEST_SIZE, USER_ID, TOKEN_TYPE, TOKEN_MEDIA } = DAYTICKET_CONFIG;
+
+  const expiresAt = dayjs()
+    .tz("Europe/Copenhagen")
+    .startOf('day')
+    .toDate();
+
+  const data = Buffer.alloc(12);
+  data.writeUInt32BE(USER_ID, 0);
+  data.writeUInt32BE(Math.floor(expiresAt.getTime() / 1000), 4);
+  data.writeUInt16BE(TOKEN_TYPE, 8);
+  data.writeUInt16BE(TOKEN_MEDIA, 10);
+
+  const nonce = crypto.randomBytes(NONCE_SIZE);
+
+  const signature = crypto
+    .createHash("shake256", { outputLength: DIGEST_SIZE })
+    .update(Buffer.concat([data, nonce, SECRET]))
+    .digest();
+
+  return base45.encode(
+    new Uint8Array(Buffer.concat([data, nonce, signature]))
   );
+}
 
-  const data = await response.json();
+export async function GET() {
+  const token = generateDayTicket();
 
-  return Response.json(data);
+  return Response.json({
+    dayticket_id: 1,
+    qr_code: token,
+  });
 }
